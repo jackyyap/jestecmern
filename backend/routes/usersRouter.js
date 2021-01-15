e7c23678b0039294a6a4b3a28dbfde957580ad69
jackyyap <jacky.yap94@gmail.com>
2021-01-15 22:05:44 +0800

    auth middleware using JWT

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 const Users = require("../models/usersModel.js");
 
 router.post("/register", async (req, res) => {
@@ -17,7 +18,7 @@ router.post("/register", async (req, res) => {
         if (password.length < 8)
             return res
                 .status(400)
-                .json({ msg: "the password needs to be at least 8 characters long" });
+                .json({ msg: "The password needs to be at least 8 characters long" });
 
         if (password !== passwordCheck)
             return res
@@ -31,6 +32,8 @@ router.post("/register", async (req, res) => {
                 .json({ msg: "Account already exists" });
 
         if (!displayName) displayName = email;
+
+        // password encryption
 
         const salt = await bcrypt.genSalt();
         const passwordHash = await bcrypt.hash(password, salt);
@@ -85,6 +88,37 @@ router.post("/login", async (req, res) => {
         console.log(token)
     }
     catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.delete("/delete", auth, async (req, res) => {
+    try {
+        const deletedUser = await Users.findByIdAndDelete(req.user);
+        res.json(deletedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.post("/tokenIsValid", async (req, res) => {
+    try {
+        const token = req.header("x-auth-token");
+        if (!token)
+            return res
+                .json(false);
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET)
+        if (!verified)
+            return res
+                .json(false);
+
+        const user = await Users.findById(verified.id);
+        if (!user)
+            return res.json(false);
+
+        return res.json(true);
+    } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
