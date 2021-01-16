e6847c1ace2c5be4eeb412e6923bd842991b1ad0
jackyyap <jacky.yap94@gmail.com>
2021-01-16 22:11:49 +0800

    backend: delete user, frontend: home screen and header with routing

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from './components/layouts/Header';
+import Home from './components/pages/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+import "./style.css";
+
+
+export default function App() {
+    return <>
+        <BrowserRouter>
+            <Header />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+            </Switch>
+        </BrowserRouter>
+    </>;
+}
