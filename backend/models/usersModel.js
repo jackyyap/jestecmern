7573cb470fe8bb9b256180e064c8eb6c719abf53
jackyyap <jacky.yap94@gmail.com>
2021-01-18 13:24:45 +0800

    added user role to model with default value upon register

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 8 },
-    displayName: { type: String }
+    displayName: { type: String },
+    userRole: {type: Array, required: true, default: "author"}
 });
 
 module.exports = Users = mongoose.model("Users", userSchema);
