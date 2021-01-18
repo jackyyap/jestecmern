651f3357fb3339f2feaef674a0db354e10d24add
jackyyap <jacky.yap94@gmail.com>
2021-01-18 13:04:25 +0800

    get user context data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Axios from "axios";
 import Header from './components/layouts/Header';
 import Home from './components/pages/Home';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import UserContext from "./context/UserContext";
 
 import "./style.css";
 
 
 export default function App() {
+    const [userData, setUserData] = useState({
+        token: undefined,
+        user: undefined,
+    });
+    //use react hook to check for token 
+    useEffect(() => {
+        const checkLoggedIn = async () => {
+            let token = localStorage.getItem("auth-token");
+            if (token === null) {
+                localStorage.setItem("auth-token", "");
+                token = "";
+            }
+            //send to the backend to validate
+
+            const tokenRes = await Axios.post("http://localhost:5000/users/tokenIsValid",
+                null,
+                { headers: { "x-auth-token": token } }
+            );
+
+            if (tokenRes.data) {
+                const userRes = await Axios.get("http://localhost:5000/users/", {
+                    headers: { "x-auth-token": token },
+                });
+                setUserData({
+                    token,
+                    user: userRes.data,
+                })
+            }
+        }
+
+
+
+        checkLoggedIn();
+    }, []);
+
+    //wrap components in User context provider to validate user before showing content
     return <>
         <BrowserRouter>
-            <Header />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-            </Switch>
+            <UserContext.Provider value={{ userData, setUserData }}>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+            </UserContext.Provider>
         </BrowserRouter>
     </>;
 }
