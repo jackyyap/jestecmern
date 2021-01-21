ab11f86e63f67b77732c0e9a6b297d9b63435304
jackyyap <jacky.yap94@gmail.com>
2021-01-21 14:23:32 +0800

    fixed login

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,49 +1,90 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import UserContext from "../../context/UserContext";
 import Axios from "axios";
 
+
 export default function Login() {
-    const register = () => history.push("/register");
-    const {setUserData} = useContext(UserContext);
-    const history = useHistory();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-    return (
-    	<header class="masthead">
+  const { setUserData } = useContext(UserContext);
+  const history = useHistory();
 
-        <div class="container h-100">
-          <div class="row h-100">
-            <div class="card rounded col-md-6 col-lg-7 mx-auto my-auto text-dark p-5">
-              <div class="justify-content-between card bg-danger text-center mb-3">
-				<div class="py-5">
-              		<h3 class="display-4 text-light">Welcome back!</h3>
-				</div>
-			  </div>
-              <form>
-                <div class="form-label-group mb-2">
-                  <label for="inputEmail">Email address</label>
-                  <input type="email" id="inputEmail" name="uid" class="form-control" placeholder="Email address" required autofocus />
-                </div>
+  const submit = async (e) => {
+    e.preventDefault();
+      const loginUser = { email, password };
+      const loginRes = await Axios.post(
+        "http://localhost:5000/users/login",
+        loginUser
+      );
+      setUserData({
+        token: loginRes.data.token,
+        user: loginRes.data.user,
+      });
+      localStorage.setItem("auth-token", loginRes.data.token);
+      history.push("/");
+    };
+  return (
+    <div className="page">
+      <h2>Log in</h2>
+      <form className="form" onSubmit={submit}>
+        <label htmlFor="login-email">Email</label>
+        <input
+          id="login-email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-                <div class="form-label-group mb-2">
-                  <label for="inputPassword">Password</label>
-                  <input type="password" id="inputPassword" name="pwd" class="form-control" placeholder="Password" required />
-                </div>
+        <label htmlFor="login-password">Password</label>
+        <input
+          id="login-password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-                <div class="custom-control custom-checkbox mb-3">
-                  <input type="checkbox" class="custom-control-input" id="customCheck1" />
-                  <label class="custom-control-label" for="customCheck1">Remember password</label>
-                </div>
-                <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase mb-2" type="submit">Login</button>
-                <hr />
-                <button class="btn btn-lg btn-secondary btn-block btn-login text-uppercase mb-2" type="button" onClick={register}>Register</button>
-                <div class="text-center">
-                  <a class="small" href="#">Forgot password?</a></div>
-              </form>
-            </div>
-          </div>
-  		</div>
+        <input type="submit" value="Log in" />
+      </form>
+    </div>
+  );
+};
+
+//     return (
+//     	<header class="masthead">
+
+//         <div class="container h-100">
+//           <div class="row h-100">
+//             <div class="card rounded col-md-6 col-lg-7 mx-auto my-auto text-dark p-5">
+//               <div class="justify-content-between card bg-danger text-center mb-3">
+// 				<div class="py-5">
+//               		<h3 class="display-4 text-light">Welcome back!</h3>
+// 				</div>
+// 			  </div>
+//               <form>
+//                 <div class="form-label-group mb-2">
+//                   <label for="inputEmail">Email address</label>
+//                   <input type="email" id="inputEmail" name="uid" class="form-control" placeholder="Email address" required autofocus />
+//                 </div>
+
+//                 <div class="form-label-group mb-2">
+//                   <label for="inputPassword">Password</label>
+//                   <input type="password" id="inputPassword" name="pwd" class="form-control" placeholder="Password" required />
+//                 </div>
+
+//                 <div class="custom-control custom-checkbox mb-3">
+//                   <input type="checkbox" class="custom-control-input" id="customCheck1" />
+//                   <label class="custom-control-label" for="customCheck1">Remember password</label>
+//                 </div>
+//                 <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase mb-2" type="submit">Login</button>
+//                 <hr />
+//                 <button class="btn btn-lg btn-secondary btn-block btn-login text-uppercase mb-2" type="button" onClick={register}>Register</button>
+//                 <div class="text-center">
+//                   <a class="small" href="#">Forgot password?</a></div>
+//               </form>
+//             </div>
+//           </div>
+//   		</div>
     
-	  	</header>
-    );
-}
+// 	  	</header>
+//     );
+// }
