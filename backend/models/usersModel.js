40f4455033c2d4adcd7d2c6bb1cbb6dcda26cd58
jackyyap <jacky.yap94@gmail.com>
2021-02-10 19:05:33 +0800

    changed role type to string

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
     affiliation: { type: String },
     title: { type: String },
     orcidId: { type: String },
-    userRole: {type: Array, required: true, default: "author"}
+    userRole: {type: String, required: true, default: "author"}
 });
 
 module.exports = Users = mongoose.model("Users", userSchema);
